5c0d6011c3371ca1715f3aa3265ad8121659fa25
Tatsuya Teshima <terima666tatsuya@gmail.com>
2021-11-20 23:18:03 +0900

    list buckets

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Tatsuya-Teshima/go-s3-bucket-list
+
+go 1.16
+
+require github.com/aws/aws-sdk-go v1.42.9
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	sess := session.Must(session.NewSessionWithOptions(
+		session.Options{
+			Profile:           "tteshima",
+			SharedConfigState: session.SharedConfigEnable,
+		},
+	))
+	svc := s3.New(sess)
+	input := &s3.ListBucketsInput{}
+
+	result, err := svc.ListBuckets(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	for _, v := range result.Buckets {
+		fmt.Println(aws.StringValue(v.Name))
+	}
+
+}
